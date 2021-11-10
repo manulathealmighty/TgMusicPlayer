5d587501dd15a7f559b6679196286774687b61f5
manulathealmighty <93851899+manulathealmighty@users.noreply.github.com>
2021-11-10 20:09:14 +0530

    Update private.py

--- callsmusic/private.py
+++ callsmusic/private.py
@@ -1,1 +1,173 @@
+import logging
+from time import time
+from datetime import datetime
+from VCPlayBot.config import BOT_USERNAME, BOT_NAME, ASSISTANT_NAME, OWNER_NAME, UPDATES_CHANNEL, SUPPORT_GROUP
+from VCPlayBot.helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
+from VCPlayBot.helpers.decorators import sudo_users_only
 
+logging.basicConfig(level=logging.INFO)
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>✨ **Welcome {message.from_user.first_name}** \n
+💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music on groups through the new Telegram’s voice chats !**
+💡 **Find out all the Bot’s commands and how they work by clicking on the » 📚 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀 button !**
+❓ **For information about all features of this bot, just type /help**
+</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "➕ Add me to your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                ],[
+                    InlineKeyboardButton(
+                        "❓ How to use Me", callback_data="cbhowtouse")
+                ],[
+                    InlineKeyboardButton(
+                         "📚 Commands", callback_data="cbcmds"
+                    ),
+                    InlineKeyboardButton(
+                        "💝 Donate", url=f"https://t.me/ucant_surpassmebruh")
+                ],[
+                    InlineKeyboardButton(
+                        "👥 Official Group", url=f"https://t.me/luvchaeyoung_botupdates"
+                    ),
+                    InlineKeyboardButton(
+                        "📣 Official Channel", url=f"https://t.me/RoseannePark_updates")
+                ],[
+                    InlineKeyboardButton(
+                        "Contact creator", url=f"https://t.me/ucant_surpassmebruh")
+                  
+                    )
+                ]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+
+@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        f"""✅ **bot is running**\n<b>💠 **uptime:**</b> `{uptime}`""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✨ Group", url=f"https://t.me/luvchaeyoung_botupdates"
+                    ),
+                    InlineKeyboardButton(
+                        "📣 Channel", url=f"https://t.me/RoseannePark_updates"
+                    )
+                ]
+            ]
+        )
+    )
+
+@Client.on_message(command(["help", f"help@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>👋🏻 **Hello** {message.from_user.mention()}</b>
+**Please press the button below to read the explanation and see the list of available commands !**
+⚡ __Powered by {BOT_NAME} A.I""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="❔ HOW TO USE ME", callback_data="cbguide"
+                    )
+                ]
+            ]
+        ),
+    )
+
+@Client.on_message(command(["help", f"help@{BOT_USERNAME}"]) & filters.private & ~filters.edited)
+async def help_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>💡 Hello {message.from_user.mention} Welcome to the help menu !</b>
+**In this menu you can open several available command menus, in each command menu there is also a brief explanation of each command**
+⚡ __Powered by {BOT_NAME} A.I__""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "📚 Basic Cmd", callback_data="cbbasic"
+                    ),
+                    InlineKeyboardButton(
+                        "📕 Advanced Cmd", callback_data="cbadvanced"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "📘 Admin Cmd", callback_data="cbadmin"
+                    ),
+                    InlineKeyboardButton(
+                        "📗 Sudo Cmd", callback_data="cbsudo"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "📙 Owner Cmd", callback_data="cbowner"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "📔 Fun Cmd", callback_data="cbfun"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+async def ping_pong(client: Client, message: Message):
+    start = time()
+    m_reply = await message.reply_text("pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        "🏓 `PONG!!`\n"
+        f"⚡️ `{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 bot status:\n"
+        f"• **uptime:** `{uptime}`\n"
+        f"• **start time:** `{START_TIME_ISO}`"
+    )
